b2242a48b43cd0566ad76e8548a191e1153dcd2d
LeonidLukin <leonidlukin90@gmail.com>
2022-08-25 16:57:42 +0300

    add toggle disable attribute

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,8 @@
 const btnStart = document.querySelector('[data-start]')
 const btnStop = document.querySelector('[data-stop]')
 btnStop.setAttribute('disabled', 'disabled')
+btnStart.setAttribute('disabled', 'disabled')
+btnStart.disabled = false
 
 let timerId = null;
 
@@ -8,7 +10,7 @@ btnStart.addEventListener('click', getRandomHexColor)
 btnStop.addEventListener('click', stopRandomHexColor)
 
 function getRandomHexColor() {
-  btnStart.setAttribute("disabled", "disabled")
+  btnStart.disabled = true
   btnStop.disabled = false
   timerId = setInterval(() => {
     const newColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`
