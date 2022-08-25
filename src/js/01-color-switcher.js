c9d18e0f5a412bbbc90ec4abf2483ea8314aa7a3
LeonidLukin <leonidlukin90@gmail.com>
2022-08-25 16:50:33 +0300

    Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const btnStart = document.querySelector('[data-start]')
+const btnStop = document.querySelector('[data-stop]')
+btnStop.setAttribute('disabled', 'disabled')
 
+let timerId = null;
+
+btnStart.addEventListener('click', getRandomHexColor)
+btnStop.addEventListener('click', stopRandomHexColor)
+
+function getRandomHexColor() {
+  btnStart.setAttribute("disabled", "disabled")
+  btnStop.disabled = false
+  timerId = setInterval(() => {
+    const newColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`
+    document.body.style.background = newColor
+  }, 1000);
+}
+
+function stopRandomHexColor() {
+  btnStart.disabled = false
+  btnStop.disabled = true
+  clearInterval(timerId)
+}
